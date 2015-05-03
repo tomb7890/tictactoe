d5aba58e745173cc7466f5e66e3a30851a27e82e
Tom Billingsley <tom.billingsley@gmail.com>
2015-05-03 14:48:23 -0400

    first version of working game

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 var xSym = "X";
 var oSym = "O";
-
-
 var count = 0;
-$('td').on('click', function() {
 
+$('td').on('click', function() {
     if (  $(this).text() == xSym ||
           $(this).text() == oSym )
     {
         return;
     }
 
+    if ( gameHasBeenWon() )
+        return;
+
     if ( nextTurn ()) {
-        // $(this).html("&#1002;");
         $(this).text(oSym);
     } else {
-        // $(this).html("&#510;");
         $(this).text(xSym);
     }
     count++;
@@ -31,19 +30,41 @@ function rowcol(r, c ) {
     return $( "tr:nth-child(" + r + ") td:nth-child(" + c + ")" );
 }
 
-function checkForDraw()
-{
+function countOfEmptyCells() {
     var emptycellcount = 0;
     for ( var tr = 1; tr < 4; tr++ ) {
         for ( var td = 1; td < 4 ; td++ ) {
             var rct = rowcol(tr,td).text();
-            console.log(rct);
+            // console.log(rct);
             if ( rct.length < 1  ) {
-                emptycellcount++;
+                emptycellcount = emptycellcount + 1;
             }
         }
     }
-    if ( emptycellcount == 0 ) {
+    return emptycellcount;
+}
+
+function gameHasBeenWon() {
+    var gameHasBeenWon = false;
+    var players = [xSym, oSym];
+    for (var p = 0; p < players.length; p++) {
+        var pp = players[p];
+        if (checkRowsForWinBy(pp) || checkColsForWinBy(pp) || checkDiagsforWinBy(pp))
+        {
+            gameHasBeenWon = true;
+        }
+    }
+    return gameHasBeenWon;
+}
+
+function noOnehasWon()
+{
+    return (!gameHasBeenWon());
+}
+
+function checkForDraw()
+{
+    if ( (countOfEmptyCells() == 0 ) && (true == noOnehasWon())  ){
         return true;
     }
     return false;
@@ -72,7 +93,7 @@ function checkDiagsforWinBy(x)
         }
     }
     if ( n == 3 ) {
-        console.log( "got here");
+        // console.log( "got here");
         return true;
     }
     return false;
@@ -83,13 +104,13 @@ function checkColsForWinBy(x) {
         var n = 0;
         for ( var tr = 1; tr < 4 ; tr++ ) {
             var rct = rowcol(tr,td).text();
-            console.log(rct);
+            // console.log(rct);
             if ( x == rct ) {
                 n = n + 1 ;
             }
         }
         if ( n == 3 ) {
-            console.log( "got here");
+            // console.log( "got here");
             return true;
         }
     }
@@ -101,13 +122,13 @@ function checkRowsForWinBy(x) {
         var n = 0;
         for ( var td = 1; td < 4 ; td++ ) {
             var rct = rowcol(tr,td).text();
-            console.log(rct);
+            // console.log(rct);
             if ( x == rct ) {
                 n = n + 1 ;
             }
         }
         if ( n == 3 ) {
-            console.log( "got here");
+            // console.log( "got here");
             return true;
         }
     }
@@ -115,27 +136,25 @@ function checkRowsForWinBy(x) {
 }
 
 function checkBoard() {
+
     var players = [xSym, oSym];
-
     for (var p = 0; p < players.length; p++) {
-        var a = checkRowsForWinBy(players[p]);
-        var b = checkColsForWinBy(players[p]);
-        var c = checkDiagsforWinBy(players[p]);
-        if ( a == true || b == true || c == true ) {
-            var message = "Player " + players[p] + " has won!";
-            myConsole(message);
-        }
+        var pp = players[p];
+        if (checkRowsForWinBy(pp))       myConsole("Player " + pp + " has won!" );
+        else if (checkColsForWinBy(pp))  myConsole("Player " + pp + " has won!" );
+        else if (checkDiagsforWinBy(pp)) myConsole("Player " + pp + " has won!" );
     }
 
-    var draw = checkForDraw();
-    if ( draw ) {
-        myConsole( "The game is a draw!" );
-
+    if (checkForDraw()) {
+        myConsole("The game is a draw!");
     }
-
 }
+
+
+
 
 function myConsole(message) {
     var rc = $("table:nth-child(1)");
+    var msg = message + "<BR>reload the page to restart.";
     rc.append("<span class='big'> - " + message + "</span>");
 }
